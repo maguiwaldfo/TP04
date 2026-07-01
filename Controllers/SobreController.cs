0351f3024775a8792cd7719173131e457cd2aa24
MAGALI WALDFOGIEL <49553387@ort>
2026-07-01 08:47:34 -0300

    creacion models y controllers

---
+++ Controllers/SobreController.cs
@@ -0,0 +1,14 @@
+namespace TP04.Controllers;
+
+public class SobreController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Abrir()
+    {
+        return View();
+    }
+}
