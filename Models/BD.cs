0bcbf0ff7ae76327a395347854bdcf524646b035
MAGALI WALDFOGIEL <49553387@ort>
2026-07-01 08:56:04 -0300

    creacion Base de Datos

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,5 +1,8 @@
 using Microsoft.Data.SqlClient;
 using Dapper; 
+public static class BD{
 
 private string _connectionString = @"Server=localhost; DataBase= Album; Integrated Security= True; TrustServerCertificate= True;"
 
+
+}
